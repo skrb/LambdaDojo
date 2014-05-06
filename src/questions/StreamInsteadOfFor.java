c8bf1b801f90831b2a2abeacc5b860ffe1e5b7fd
yuichi <yuichi@192.168.100.101>
2014-05-06 15:06:34 +0900

    Fixed minor error.

--- src/questions/StreamInsteadOfFor.java
+++ src/questions/StreamInsteadOfFor.java
@@ -34,7 +34,7 @@ public class StreamInsteadOfFor {
     private void simpleStream2() {
         List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        int ave = 0;
+        double ave = 0.0;
         for (Integer x : numbers) {
             ave += x;
         }
