c9bbbc4a42df52888019ce1a420f57484a5227ef
Yuichi.Sakuraba <yuichi.sakuraba@gmail.com>
2015-03-31 21:25:42 +0900

    サンプルの構成を変更
    サンプルの構成を変更

---
+++ projects/NetBeans/LambdaDojo/src/questions/section2/S02LambdaInsteadOfAnonClass.java
@@ -0,0 +1,48 @@
+package questions.section2;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class S02LambdaInsteadOfAnonClass {
+
+    public S02LambdaInsteadOfAnonClass() {
+        rewriteByLambda();
+    }
+
+    // 2-2 Lambda 式で書きかえてみましょう
+    private void rewriteByLambda() {
+
+        // この匿名クラスを Lambda 式で書きかえ
+        Callable<List<String>> callable = new Callable<List<String>>() {
+            @Override
+            public List<String> call() throws Exception {
+                // ファイル読み込み処理
+                Path path = Paths.get("src/questions/section2/S02LambdaInsteadOfAnonClass.java");
+                return Files.readAllLines(path);
+            }
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+
+        // 非同期にファイルを読み込み
+        Future<List<String>> future = executor.submit(callable);
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException ex) {
+            // 例外処理
+        } finally {
+            executor.shutdown();
+        }
+    }
+
+    public static void main(String... args) {
+        new S02LambdaInsteadOfAnonClass();
+    }
+}
---
+++ projects/NetBeans/LambdaDojo/src/questions/section2/S05LambdaForStrategy.java
@@ -0,0 +1,56 @@
+package questions.section2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Consumer;
+
+public class S05LambdaForStrategy {
+
+    static class Container<T> {
+
+        private T value;
+
+        public Container(T value) {
+            this.value = value;
+        }
+
+        public void print(Consumer<T> saver) {
+            saver.accept(value);
+        }
+    }
+    
+    public S05LambdaForStrategy() {
+        useContainer();
+    }
+    
+    private void useContainer() {
+        Container<String> container = new Container<>("Hello, World!");
+        
+        container.print(new Consumer<String>() {
+            @Override
+            public void accept(String value) {
+                System.out.println(value);
+            }
+        });
+        
+        final String filename = "container.txt";
+        container.print(new Consumer<String>() {
+            @Override
+            public void accept(String value) {
+                try {
+                    Path path = Paths.get(filename);
+                    Files.write(path, value.getBytes());
+                } catch (IOException ex) {
+                    // 例外処理
+                    System.err.println("printに失敗しました");
+                }
+            }
+        });
+    }
+
+    public static void main(String... args) {
+        new S05LambdaForStrategy();
+    }
+}
